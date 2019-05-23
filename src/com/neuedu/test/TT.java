9f640e84d06f642980550145a84c8917d126a279
zsxllx0208 <644148625@qq.com>
2019-05-23 18:45:36 +0800

    作业

---
+++ src/com/neuedu/test/TT.java
@@ -0,0 +1,10 @@
+package com.neuedu.test;
+
+public class TT {
+    public static void main(String[] args) {
+        System.out.println("hello word");
+        int a=1;
+        int b=a+1;
+        System.out.println(b);
+    }
+}
