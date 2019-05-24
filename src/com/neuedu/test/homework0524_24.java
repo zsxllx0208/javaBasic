25a92a150588fdf668926532e464b9de13d9a66b
zsxllx0208 <644148625@qq.com>
2019-05-24 19:52:35 +0800

    作业

---
+++ src/com/neuedu/test/homework0524_24.java
@@ -0,0 +1,14 @@
+package com.neuedu.test;
+
+public class homework0524_24 {
+    public static void main(String[] args) {
+        int a=9,b=1;
+        for (a=9;a>0;a--){
+            b*=a;
+        }
+        System.out.println(b);
+
+
+
+    }
+}
