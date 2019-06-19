86951b81fa23b081f9cc45b398020d79640e98ed
zsxllx0208 <644148625@qq.com>
2019-06-19 19:33:06 +0800

    作业

--- src/com/neuedu/test1/Brass.java
+++ src/com/neuedu/test1/Brass.java
@@ -4,6 +4,8 @@ public class Brass extends Instrument {
 
 
     public void play(){
+        super.play();
         System.out.println("铜管演奏方法");
+
     }
 }
--- src/com/neuedu/test1/Wind.java
+++ src/com/neuedu/test1/Wind.java
@@ -4,6 +4,7 @@ public class Wind extends Instrument {
 
 //    @Override
     public void play(){
+        super.play();
         System.out.println("风琴演奏方式");
     }
 
